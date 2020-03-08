5fa994068055b79b40e9612f4196a033ff79f989
Oriana Gonzalez <go.oriana@correo.unimet.edu.ve>
2020-03-08 11:58:20 -0400

    nuevo archivo

---
+++ FuncionesComunes.py
@@ -0,0 +1,15 @@
+def validar_entero_positivo(n):
+    es_entero = False
+    try:
+        int(n)
+        if int(n) >= 0:
+            es_entero = True
+    except:
+        es_entero = False
+    return es_entero
+
+def pedir_entero_positivo_validado(mensaje):
+    n = input(mensaje)
+    while not validar_entero_positivo(n):
+        n = input(mensaje)
+    return int(n)
